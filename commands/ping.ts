fb6147308cef118037fbd989095e04dff4d2deb9
BuildTools <cat71602@gmail.com>
2019-05-07 09:22:21 -0400

    maybe works

---
+++ commands/ping.ts
@@ -0,0 +1,17 @@
+import { CommandCreateInfo } from "cat16-bot-core";
+
+const createInfo: CommandCreateInfo = {
+  run(context) {
+    const now = new Date();
+    context.msg.channel.send("Ping: loading...").then(sent => {
+      const msg = sent instanceof Array ? sent[0] : sent;
+      const then = new Date();
+      msg.edit(`Ping: ${then.getTime() - now.getTime()}ms`);
+    });
+  },
+  async hasPermission(context) {
+    return true;
+  }
+};
+
+export default createInfo;
